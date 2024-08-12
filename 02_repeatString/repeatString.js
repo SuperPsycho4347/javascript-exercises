bfcb347bd079858b2ec985647ad33cff5f3641fc
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-13 04:06:52 +0500

    repeatString: passes 5 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatString = function(myString, repeatInterval) {
+    if (repeatInterval === 0) {
+        return ''
+    }
+    else if (repeatInterval <= 0) {
+        return 'ERROR'
+    }
+    else {
+        let i = 1
+        let currentString = myString
+        while(i < repeatInterval) {
+            i++
+            currentString = currentString + myString
+        }
+        return currentString
+    }
 };
+
+// const myString = 'afaq'
+// const myNum = 3
+
+repeatString('myString', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
