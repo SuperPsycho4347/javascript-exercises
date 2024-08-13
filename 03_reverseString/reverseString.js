1411c1e093a1ce93977c1aad0f935b1554713d15
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-13 19:39:14 +0500

    reverseString: passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+    let splitString = myString.split("")
 
+    let reversedArray = splitString.reverse()
+
+    let joinedArray = reversedArray.join("")
+
+    return joinedArray
 };
+
+let myString = 'hello'
+reverseString(myString)
 
 // Do not edit below this line
 module.exports = reverseString;
