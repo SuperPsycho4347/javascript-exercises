d7253bd62a2bc1fc46b8dd229b3428fc5a635bcc
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-14 01:05:54 +0500

    sumAll: completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let mySum = 0
+    if (startNum < 0) {
+        return 'ERROR'
+    }
+    else if (Number.isInteger(startNum) == false || Number.isInteger(endNum) == false) {
+        return 'ERROR'
+    }
+    else if (startNum < endNum) {
+        while (startNum <= endNum) {
+            mySum = mySum + startNum
+            startNum++
+        }
+        return mySum
+    }
+    else if (startNum > endNum) {
+        while (startNum >= endNum) {
+            mySum = mySum + startNum
+            startNum--
+        }
+        return mySum
+    }
 };
+let firstNum, secondNum
+sumAll(firstNum, secondNum)
 
 // Do not edit below this line
 module.exports = sumAll;
