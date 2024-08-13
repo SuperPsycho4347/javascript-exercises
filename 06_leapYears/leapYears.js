3b5950116a48f232af1a75045f62c9348240668f
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-14 01:16:34 +0500

    leapYears: completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if (givenYear % 100 !== 0 && givenYear % 4 === 0 || givenYear % 400 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
 };
+
+leapYears(2004)
 
 // Do not edit below this line
 module.exports = leapYears;
