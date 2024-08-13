fcce31f39d6bd8f39368003537e5873377510cfa
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-14 01:47:00 +0500

    calculator: passed multiplication tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,31 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  let sum = firstNum + secondNum
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	let diff = firstNum - secondNum
+  return diff
 };
 
-const sum = function() {
-	
+const sum = function(givenNums) {
+	let sum = 0
+  let i = 0
+  while (i < givenNums.length) {
+    sum = sum + givenNums[i]
+    i++
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(givenNums) {
+  let product = givenNums[0]
+  let i = 1
+  while (i < givenNums.length) {
+    product = product * givenNums[i]
+    i++
+  }
+  return product
 };
 
 const power = function() {
@@ -21,6 +35,12 @@ const power = function() {
 const factorial = function() {
 	
 };
+
+// Function calls
+add(1, 2)
+subtract(1, 2)
+sum([1, 2])
+multiply([1,2])
 
 // Do not edit below this line
 module.exports = {
