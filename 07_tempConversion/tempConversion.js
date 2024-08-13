6685935d1ea6507ea56b412d84c032e1eb82e477
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-14 01:25:40 +0500

    tempConversion: completed, passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenTemp) {
+  tempConversion = Math.round(((givenTemp - 32) * 5/9) * 10) / 10
+  return tempConversion
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenTemp) {
+  tempConversion = Math.round((givenTemp * 9/5 + 32) * 10) / 10
+  return tempConversion
 };
 
+convertToCelsius(32)
+convertToFahrenheit(32)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
