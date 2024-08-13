1641eb213231f217632319f94b8838318746b661
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-08-13 20:52:05 +0500

    removeFromArray: completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...myNums) {
+    const result =  myArray.filter(element => !myNums.includes(element))
+    return result
 };
+
+removeFromArray([1,2,3,4], 1)
 
 // Do not edit below this line
 module.exports = removeFromArray;
