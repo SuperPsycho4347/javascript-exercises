c657dd96296ee6a209bec7234f9ab0ac00425e06
Afaq Ahmad <superpsycho4347@gmail.com>
2024-08-15 11:38:47 +0500

    fibbonacci: passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(lastNum) {
+   let num1, num2, nextTerm = 0, i = 1
+   num1 = 1, num2 = 1
+    while (i <= lastNum) {
+        num1 = num2
+        num2 = nextTerm
+        nextTerm = num1 + num2
+        i++
+    }
+    return nextTerm
 };
 
+fibonacci(2)
 // Do not edit below this line
 module.exports = fibonacci;
