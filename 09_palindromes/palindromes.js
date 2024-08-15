22130ccceae2a63c26a01ffe4c2fa45c6c98fde7
Afaq Ahmad <superpsycho4347@gmail.com>
2024-08-15 10:24:09 +0500

    palindromes: completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (myStr) {
+    let re = /[\W_]/g
 
+    let lowRegStr = myStr.toLowerCase().replace(re, '')
+
+    let reverseStr = lowRegStr.split('').reverse().join('')
+
+    return reverseStr == lowRegStr
 };
 
+palindromes('racecar')
 // Do not edit below this line
 module.exports = palindromes;
