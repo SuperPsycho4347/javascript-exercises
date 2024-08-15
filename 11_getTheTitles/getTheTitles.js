0871317446bd928a1421742f3fb534bb09cc7b40
Afaq Ahmad <superpsycho4347@gmail.com>
2024-08-15 11:42:52 +0500

    getTheTitles: completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = books.map(a => a.title)
+    return titles
 };
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
